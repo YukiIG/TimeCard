1a349008a563823d9c0290323bba4763958efd73
yk <yk1203.i@docomo.ne.jp>
2016-03-04 16:57:08 +0900

    時間差分取得できるようになった

--- TimeCard/DetailViewController.swift
+++ TimeCard/DetailViewController.swift
@@ -11,10 +11,12 @@ import Parse
 
 class DetailViewController: UIViewController {
     var userId: String!
+    let now = NSDate()
     var startTime: NSDate!
     var endTime: NSDate!
     var workTime: Int = 0
-    @IBOutlet var idLabel: UILabel!
+    @IBOutlet var hourLabel: UILabel!
+    @IBOutlet var wageLabel: UILabel!
     let defaults:NSUserDefaults = NSUserDefaults.standardUserDefaults()
 
     override func viewDidLoad() {
@@ -32,18 +34,39 @@ class DetailViewController: UIViewController {
                         print(object)
                         self.startTime = object.createdAt
                         self.endTime = object.updatedAt
-                        print(self.startTime)
-                        
+                        print(self.hour(self.startTime))
+                        print(self.hour(self.now))
+                        var time = Int(self.endTime.timeIntervalSinceDate(self.startTime))
+
+                        //今月の給料
+                        if self.month(self.startTime) == self.month(self.now){
+                        self.workTime += time
+                        }
+                        print(time)
                     }
-                    
+                    print(self.workTime)
                 } else {
                         print(error)
                 }
             }
+
+        
         
     }
+    func month(date : NSDate) -> Int {
+        let calendar = NSCalendar.currentCalendar()
+        var comp : NSDateComponents = calendar.components(
+            NSCalendarUnit.Month, fromDate: date)
+        return comp.month
+    }
+
+    func hour(date : NSDate) -> Int {
+        let calendar = NSCalendar.currentCalendar()
+        var comp : NSDateComponents = calendar.components(
+            NSCalendarUnit.Hour, fromDate: date)
+        return comp.month
+    }
     
- 
 //    override func viewWillAppear(animated: Bool) {
 //        super.viewWillAppear(animated)
 //
